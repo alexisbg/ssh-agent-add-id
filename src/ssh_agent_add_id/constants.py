c196b878c4469f5adbeadb7a3df10011fdbfdbd8
Alexis Bergue <alexis.bergue@gmail.com>
2023-12-21 18:44:56 +0100

    Update typing of constants

--- src/ssh_agent_add_id/constants.py
+++ src/ssh_agent_add_id/constants.py
@@ -1,3 +1,8 @@
-APP_DESCRIPTION = "Checks whether an identity file has already been stored by the SSH agent. \
-    If not, it adds this identity by prompting for its passphrase."
-APP_NAME = "ssh-agent-add-id"
+from typing import Final
+
+
+# fmt: off
+APP_DESCRIPTION: Final[str] = "Checks whether an identity file has already been stored by the SSH\
+    agent. If not, it adds this identity by prompting for its passphrase."
+# fmt: on
+APP_NAME: Final[str] = "ssh-agent-add-id"
