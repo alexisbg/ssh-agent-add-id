e19d562f2b3e5a3826551728c5cc8674ee45e02f
Alexis Bergue <alexis.bergue@gmail.com>
2023-12-23 21:35:11 +0100

    Clean APP_DESCRIPTION string

--- src/ssh_agent_add_id/constants.py
+++ src/ssh_agent_add_id/constants.py
@@ -1,8 +1,6 @@
 from typing import Final
 
 
-# fmt: off
-APP_DESCRIPTION: Final[str] = "Checks whether an identity file has already been stored by the SSH\
-    agent. If not, it adds this identity by prompting for its passphrase."
-# fmt: on
+APP_DESCRIPTION: Final[str] = "Checks whether an identity file has already been stored by the SSH \
+agent. If not, it adds this identity by prompting for its passphrase."
 APP_NAME: Final[str] = "ssh-agent-add-id"
